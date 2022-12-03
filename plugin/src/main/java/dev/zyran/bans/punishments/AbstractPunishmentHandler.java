5d5029f497195e81e8f1c1c95ec87eebe907e73b
Zyran <zyran.dev@gmail.com>
2022-12-02 21:25:21 -0600

    feat(punishment): add PunishmentHandler abstract implementation

---
+++ plugin/src/main/java/dev/zyran/bans/punishments/AbstractPunishmentHandler.java
@@ -0,0 +1,52 @@
+package dev.zyran.bans.punishments;
+
+import dev.zyran.api.punishment.Punishment;
+import dev.zyran.api.punishment.PunishmentHandler;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.time.Duration;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+public abstract class AbstractPunishmentHandler
+		implements PunishmentHandler {
+
+	@Override
+	public CompletableFuture<Punishment> execute(
+			@NotNull final UUID userId,
+			@Nullable final String reason,
+			@NotNull final UUID actorId,
+			@Nullable final Duration duration
+	) {
+		return save(new SimplePunishment(userId, reason, actorId, duration))
+				       .thenApplyAsync(punishment -> {
+					       execute(punishment);
+					       return punishment;
+				       });
+	}
+
+	@Override
+	public CompletableFuture<Punishment> sendExecutedPunishment(
+			@NotNull final Player sender,
+			@NotNull final UUID userId,
+			@Nullable final String reason,
+			@NotNull final UUID actorId,
+			@Nullable final Duration duration
+	) {
+		return execute(userId, reason, actorId, duration)
+				       .whenComplete((punishment, throwable) -> {
+					       if (throwable != null) {
+						       sendFailureMessage(sender, throwable);
+						       return;
+					       }
+					       sendSuccessMessage(sender, punishment);
+				       });
+	}
+
+	protected abstract void execute(final @NotNull Punishment punishment);
+	protected abstract CompletableFuture<Punishment> save(@NotNull Punishment punishment);
+	protected abstract void sendSuccessMessage(@NotNull Player sender, @NotNull Punishment punishment);
+
+	protected abstract void sendFailureMessage(@NotNull Player sender, @Nullable Throwable throwable);
+}

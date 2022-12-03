3ff2e09e83cce6ee95c1fc866c9a8e2842d08ec3
Zyran <zyran.dev@gmail.com>
2022-12-02 21:24:05 -0600

    feat(build): * add unnamed team maven repository * added libraries: - nmessage - nmessage yaml (message source) - inject - trew

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -5,10 +5,14 @@ plugins {
 repositories {
     mavenCentral()
     maven("https://repo.codemc.io/repository/nms/")
+    maven("https://repo.unnamed.team/repository/unnamed-public/")
 }
 
 dependencies {
     implementation(project(":api"))
+    implementation(libs.inject.trew)
+    implementation(libs.nmessage)
+    implementation(libs.nmessage.yaml)
     compileOnly(libs.spigot) {
         exclude(group = "net.md-5", module = "bungeecord-chat")
     }

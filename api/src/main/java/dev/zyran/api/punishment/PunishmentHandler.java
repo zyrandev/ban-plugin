69dc04b09196fd66f904cb2f7992a281ba3bdfed
Zyran <zyran.dev@gmail.com>
2022-12-02 21:24:54 -0600

    feat(punishment): add PunishmentHandler abstraction

---
+++ api/src/main/java/dev/zyran/api/punishment/PunishmentHandler.java
@@ -0,0 +1,26 @@
+package dev.zyran.api.punishment;
+
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.time.Duration;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+public interface PunishmentHandler {
+
+	CompletableFuture<Punishment> execute(
+			@NotNull UUID userId,
+			@Nullable String reason,
+			@NotNull UUID actorId,
+			@Nullable Duration duration
+	);
+
+	CompletableFuture<Punishment> sendExecutedPunishment(
+			@NotNull Player sender,
+			@NotNull UUID userId,
+			@Nullable String reason,
+			@NotNull UUID actorId,
+			@Nullable Duration duration
+	);
+}

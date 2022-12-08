491412f9dc243750d77a4de0b71a82611fe31ea7
Zyran <zyran.dev@gmail.com>
2022-12-07 23:21:52 -0600

    feat(storage): added methods - save punishment - retrieve user - retrieve punishment - retrieve all user punishment's

--- api/src/main/java/dev/zyran/api/storage/Storage.java
+++ api/src/main/java/dev/zyran/api/storage/Storage.java
@@ -1,11 +1,18 @@
 package dev.zyran.api.storage;
 
-import dev.zyran.api.user.UserHistoryRecord;
+import dev.zyran.api.punishment.Punishment;
+import dev.zyran.api.user.User;
 
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 public interface Storage {
 
-	CompletableFuture<Set<UserHistoryRecord>> retrieveUserHistory(UUID userId);
+	CompletableFuture<Punishment> savePunishment(String type, Punishment punishment);
+
+	CompletableFuture<User> retrieveUser(UUID userId);
+
+	CompletableFuture<Punishment> retrievePunishment(String punishmentId);
+
+	CompletableFuture<Set<Punishment>> retrieveUserPunishments(UUID userId);
 }
